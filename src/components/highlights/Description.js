dab4101ede51b0961b8af6ca39c7fd8b4f87a33a
Amit Bothra <amit.bothra@outlook.com>
2019-01-04 06:26:15 -0600

    added Highlight Description component

---
+++ src/components/highlights/Description.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+
+  return ( 
+    <Fade>
+      <div className="center_wrapper">
+         <h2>Highlights</h2>
+         <div className="highlight_description">
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris laoreet urna at quam sodales, eu fringilla sapien placerat. Aenean egestas dictum nulla tempor pellentesque. Aenean condimentum eros eros, vitae pretium tortor facilisis nec. Nullam commodo sapien ut elit iaculis, vel suscipit nulla sagittis. Cras dignissim sapien in orci malesuada rutrum. Phasellus tempus justo tincidunt vestibulum finibus. Nulla facilisis nisl eget sapien malesuada consequat. Phasellus facilisis mauris at erat volutpat vulputate. Aenean aliquet, eros nec maximus blandit, magna risus dictum metus, id iaculis urna risus eget metus. Sed posuere blandit nisi, a elementum eros mattis a. Quisque non lacinia lectus, id sagittis mi. Curabitur in hendrerit lacus, in ultricies leo.
+         </div>
+
+
+      </div>
+    </ Fade> 
+    
+  );
+};
+
+export default Description;
---
+++ src/components/highlights/Discount.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Discount extends React.Component {
+  
+  render () {
+    return(
+      <div>
+  
+      </div>
+  
+    );
+  }
+  
+}
+
+export default Discount;
