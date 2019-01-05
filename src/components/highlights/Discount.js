31901cc76e8354352a8396827f36765433f268fe
Amit Bothra <amit.bothra@outlook.com>
2019-01-04 19:25:46 -0600

    Added Highlight Discount Component

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
 
 class Discount extends React.Component {
   
+  state = {
+    discountStart:0,
+    discountEnd:30
+  }
+
+  percentage = () => {
+    if(this.state.discountStart < this.state.discountEnd) {
+      this.setState({
+        discountStart: this.state.discountStart + 1
+      })
+    }
+  }
+
+  componentDidUpdate () {
+     setTimeout( () => {
+       this.percentage()
+     },30)
+  }
+
+  
   render () {
     return(
-      <div>
-  
+      <div className="center_wrapper">
+          <div className="discount_wrapper">
+              
+              <Fade
+                onReveal={() => this.percentage()}
+              >
+                <div className="discount_percentage">
+                  <span>{this.state.discountStart}%</span>
+                  <span>OFF</span>
+                </div>
+              </Fade>
+
+              <Slide right>
+                <div className="discount_description">
+                  <h3>Purchase tickets before 20th JUNE</h3>
+                  <p>Aenean condimentum eros eros, vitae pretium tortor facilisis nec. Nullam commodo sapien ut elit iaculis, vel suscipit nulla sagittis. Cras dignissim sapien in orci malesuada rutrum. Phasellus tempus justo tincidunt vestibulum finibus.</p>
+                  <div>
+                      button
+                    </div>
+                </div>
+              </Slide>
+              
+              
+        
+          </div>
       </div>
   
     );
